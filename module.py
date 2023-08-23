c394ded92a458e15c903f5f3e380ea3345978708
Andrew S <relics3@gmail.com>
2023-08-23 16:47:31 +0300

    Code according to PEP8, Add annotations and docstrings

--- module.py
+++ module.py
@@ -1,30 +1,37 @@
+"""Модуль функций для игры ""Рассчитай и победи""."""
 from random import randint
 
 
-def set_enemy_health():
+def set_enemy_health() -> int:
+    """Устанавливает значение противника."""
     return randint(80, 120)
 
 
-def get_lite_attack():
+def get_lite_attack() -> int:
+    """Возвращает значение слабой атаки."""
     return randint(2, 5)
 
 
-def get_mid_attack():
+def get_mid_attack() -> int:
+    """Возвращает значение средней атаки."""
     return randint(15, 25)
 
 
-def get_hard_attack():
+def get_hard_attack() -> int:
+    """Возвращает значение сильной атаки."""
     return randint(30, 40)
 
 
-def compare_valumes(enemy_health, user_total_attack):
+def compare_valumes(enemy_health: int, user_total_attack: int) -> bool:
+    """Сравниевает сумму атак со здоровьем противника."""
     point_difference = abs(enemy_health - user_total_attack)
     if 0 <= point_difference <= 10:
         return True
     return False
 
 
-def get_user_attack():
+def get_user_attack() -> int:
+    """Запрашивает тип атаки и возвращает количество очков."""
     total = 0
     attacks_types = {
         'lite': get_lite_attack,
@@ -36,11 +43,12 @@ def get_user_attack():
         input_attack = input('Введи тип атаки: ').lower()
         attack_value = attacks_types[input_attack]()
         print(f'Количество очков твоей атаки: {attack_value}.')
-        total += 1
+        total += attack_value
     return total
 
 
-def run_game():
+def run_game() -> bool:
+    """Запускает игру."""
     user_total_attack = get_user_attack()
     enemy_health = set_enemy_health()
     print(f'Тобой нанесён урон противнику равный {user_total_attack}.')
@@ -52,6 +60,8 @@ def run_game():
     yes_no = {
         'Y': True,
         'N': False,
+        'y': True,
+        'n': False,
     }
     replay = input('Чтобы сыграть ещё раз, введи "y"; '
                    'если не хочешь продолжать игру, введи "n": ')
